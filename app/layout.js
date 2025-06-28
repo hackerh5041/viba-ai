981274b97da2ad357094492ede0f6b27da59f135
hackerh5041 <59780627+hackerh5041@users.noreply.github.com>
2025-06-28 18:08:59 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Viba | AI Video Generator",
+  description: "Create hyper-realistic videos with voice, visuals, and motion using advanced AI. Viba makes content creation effortless.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
