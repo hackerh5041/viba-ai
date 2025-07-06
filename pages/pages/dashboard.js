7edb9b2ade800166a777e93dd82444d90fc7f0d7
hackerh5041 <59780627+hackerh5041@users.noreply.github.com>
2025-07-05 21:32:41 -0400

    Delete pages/pages/dashboard.js

--- pages/pages/dashboard.js
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: '4rem', textAlign: 'center' }}>
-      <h1>Dashboard</h1>
-      <p>Paste your script and generate your video here.</p>
-    </div>
-  );
-}
