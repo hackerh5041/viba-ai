1bea90d94c3a32d8d22ebeb04a73bdb2efda6bfb
hackerh5041 <59780627+hackerh5041@users.noreply.github.com>
2025-07-05 21:35:49 -0400

    Delete pages/login.js

--- pages/login.js
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-  return (
-    <div style={{ padding: '4rem', textAlign: 'center' }}>
-      <h1>Login Page</h1>
-      <p>This is where users will log in or sign up.</p>
-    </div>
-  );
-}
