60d0b139fc92675a2e7c5bd9e10b29cf6f754682
hackerh5041 <59780627+hackerh5041@users.noreply.github.com>
2025-07-05 21:33:00 -0400

    Delete pages/pages/pages/pricing.js

--- pages/pages/pages/pricing.js
+++
@@ -1,8 +0,0 @@
-export default function Pricing() {
-  return (
-    <div style={{ padding: '4rem', textAlign: 'center' }}>
-      <h1>Pricing</h1>
-      <p>Access all tools with just $20/month.</p>
-    </div>
-  );
-}
