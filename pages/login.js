c482fdd91e085d2aed94225113acff73da044881
hackerh5041 <59780627+hackerh5041@users.noreply.github.com>
2025-07-05 21:52:06 -0400

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,17 @@
+import Link from 'next/link';
+
 export default function Login() {
   return (
-    <div style={{ padding: '3rem', textAlign: 'center' }}>
-      <h1>Login to VibaAI</h1>
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>🔐 Login</h1>
       <form>
-        <input type="email" placeholder="Email" required /><br /><br />
-        <input type="password" placeholder="Password" required /><br /><br />
-        <button type="submit">Login</button>
+        <input type="email" placeholder="Email" required style={{ display: 'block', marginBottom: 10 }} />
+        <input type="password" placeholder="Password" required style={{ display: 'block', marginBottom: 10 }} />
+        <button type="submit">Log In</button>
       </form>
+      <p style={{ marginTop: 20 }}>
+        Don’t have an account? <Link href="/register">Register</Link>
+      </p>
     </div>
   );
 }
