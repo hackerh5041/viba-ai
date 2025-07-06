f719a8ef291ac9963742dd11880b5efab88f13cd
hackerh5041 <59780627+hackerh5041@users.noreply.github.com>
2025-07-05 21:30:31 -0400

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div style={{ padding: '4rem', textAlign: 'center' }}>
+      <h1>Login Page</h1>
+      <p>This is where users will log in or sign up.</p>
+    </div>
+  );
+}
